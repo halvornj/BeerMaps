db8f3f088c4d666212f1e43abad84592d6402f4a
Seaweedbeard <ruben.bjorkoy@gmail.com>
2024-05-20 20:49:27 +0200

    Begun navbar

---
+++ beer_maps/lib/ui/components/navbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget {
+  @override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.list),
+            label: 'List',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
