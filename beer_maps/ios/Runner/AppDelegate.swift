2d4daa7931b09da7fa691d5d9c697adea4062cbb
halvornj <halvornjaatad@gmail.com>
2024-06-13 22:59:32 +0200

    added api key for IOS
    made it secret and invisible on github first trygit restore --staged ios/Runner/keys.plist

--- beer_maps/ios/Runner/AppDelegate.swift
+++ beer_maps/ios/Runner/AppDelegate.swift
@@ -2,13 +2,22 @@ import UIKit
 import Flutter
 import GoogleMaps
 
+
+let filePath = Bundle.main.path(forResource: "keys.plist", ofType: "plist")
+let plist = NSDictionary(contentsOfFile: filePath)
+let MAPS_API_KEY = plist?.object(forKey: "MAPS_API_KEY") as? String else {
+      fatalError("Couldn't find key 'MAPS_API_KEY' in 'keys.plist'.")
+    }
+
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
+     application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+
+    GMSServices.provideAPIKey(MAPS_API_KEY)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
